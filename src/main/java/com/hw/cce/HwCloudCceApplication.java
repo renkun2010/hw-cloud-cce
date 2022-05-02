c44d907d4f28028c7f7d7477859afe5d44fc1a7b
Renjk <529836005@qq.com>
2022-05-02 18:53:31 +0800

    version-01

--- src/main/java/com/hw/cce/HwCloudCceApplication.java
+++ src/main/java/com/hw/cce/HwCloudCceApplication.java
@@ -8,7 +8,11 @@ public class HwCloudCceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HwCloudCceApplication.class, args);
+<<<<<<< HEAD
         System.out.println("  ");
+=======
+        System.out.println("helloWord");
+>>>>>>> a2f769f (version-01)
     }
 
 }
